32536089a2552e9b02aac5a072582f5fa852107c
orangeobject <orangeobject@gmail.com>
2013-06-13 10:04:35 +0900

    Update displaytest.php

--- displaytest.php
+++ displaytest.php
@@ -1,58 +1,37 @@
- <html> 
+<!DOCTYPE html>
+<html lang="ja" xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="refresh" content="5;url=displaytest.php"> 
+<title>Display Messages</title> 
 <script type="text/javascript" src="ajax.js"> </script> 
-
 <script type="text/javascript">
-  function getspeak(){
+	function getspeak(){
 		
 		request.open("GET","./speaking.php?words=test&nick=dq",true);
 		request.send(null);
 }
 </script>
 <script type="text/javascript">
-
 	createRequest();
-
-
 	function getId(){
- 
 		msg=document.getElementById("1234").value;
-
 		alert(msg);
-	
 }
-
 	function updatePage() {
 			if (request.readyState == 4) {
-
 			document.getElementById("1123").innerHTML = request.responseText;
 		}
 	}
 </script>
-
 </head>
-<body>
-<DIV Id=1234>
-</DIV>
-
- <inputId="1234"></input>
-
-<button onClick="getId();"></button>
-
-<button onClick="getspeak();">say</botton>
-
-<button onClick="getdisplaytest();" >get</button>
-
-
-<form action="speak.php" method="post" target="_self">
-<input type="button" onclick="location.href='http://10.17.23.222/mylog'"value="datebase" target="_self"></body>
-</form> 
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
- 
-<title>Display Messages</title> 
-<meta http-equiv="refresh" content="5;url=displaytest.php"> 
-</head> 
 <body> 
+	<input Id="1234">
+	<button onClick="getId();"></button>
+	<button onClick="getspeak();">say</botton>
+	<button onClick="getdisplaytest();" >get</button>
+	<form action="speak.php" method="post" target="_self">
+	<input type="button" onclick="location.href='http://10.17.23.222/mylog'"value="datebase" target="_self">
 <?php 
 //connect to mysql server, server name: main, database username: root 
 $link_ID=mysql_connect('localhost',"root","116center"); 
@@ -74,6 +53,5 @@ $str="DELETE FROM chat WHERE chtime<'$limtime' ;" ;
 $result=mysql_query($str,$link_ID);
 mysql_close($link_ID); 
 ?> 
-
 </body> 
-</html> 
+</html>
